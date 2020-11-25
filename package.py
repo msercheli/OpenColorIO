8c3b748aa501b4922d92150e72b014d37478f08f
Marcelo Sercheli <msercheli@gmail.com>
2020-11-25 18:27:08 -0500

    Add rez files

---
+++ package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+name = "ocio"
+
+version = "2.0.0"
+
+authors = [
+    "Sony Pictures"
+]
+
+requires = [
+    "gcc",
+    "cmake",
+    "python",
+]
+
+def commands():
+    env.OCIO_ROOT = "{root}"
+    env.OCIO_LOCATION = "{root}"
+    env.OPENCOLORIO_ROOT = "{root}"
+    env.OPENCOLORIO_LOCATION = "{root}"
+    env.PATH.append("{root}/bin")
+
+uuid = "repository.ocio"
